c96d272f1e195d0cbbb607e6b3fcd802e59ce787
Evgenii Khramkov <evkhramk@microsoft.com>
2019-07-11 14:29:40 -0700

    remove copy

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,6 +1,6 @@
-ls -la "$HOME/.gradle/init.d"
+# ls -la "$HOME/.gradle/init.d"
 
-mv "$HOME/.gradle/init.d/app-center-plugin.gradle" "$HOME/.gradle/init.d/app-center-plugin.gradle_old"
-mv ./app-center-plugin.gradle "$HOME/.gradle/init.d/app-center-plugin.gradle"
+# mv "$HOME/.gradle/init.d/app-center-plugin.gradle" "$HOME/.gradle/init.d/app-center-plugin.gradle_old"
+# mv ./app-center-plugin.gradle "$HOME/.gradle/init.d/app-center-plugin.gradle"
 
 tail -n 80 "$HOME/.gradle/init.d/app-center-plugin.gradle"
