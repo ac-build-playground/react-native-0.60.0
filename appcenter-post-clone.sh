56c4848491df763abbfef4bfed5c97046c8cb461
Evgeniy Khramkov <evkhramk@microsoft.com>
2019-07-12 14:01:04 -0700

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,6 +1,5 @@
-# ls -la "$HOME/.gradle/init.d"
-
-# mv "$HOME/.gradle/init.d/app-center-plugin.gradle" "$HOME/.gradle/init.d/app-center-plugin.gradle_old"
-# mv ./app-center-plugin.gradle "$HOME/.gradle/init.d/app-center-plugin.gradle"
-
-tail -n 80 "$HOME/.gradle/init.d/app-center-plugin.gradle"
+if [ "$secret" == "secret" ]; then
+  echo "secret is valid"
+else
+  echo "secret invalid!"
+fi
